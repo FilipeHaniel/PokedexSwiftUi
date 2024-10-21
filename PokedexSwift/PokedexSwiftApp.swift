b5496b1b1372f0e1afa6686d6fbd2ef51b7ea9d3
FilipeHaniel <filipe.haniel@gmail.com>
2024-10-20 23:29:24 -0300

    first commit

---
+++ PokedexSwift/PokedexSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftApp.swift
+//  PokedexSwift
+//
+//  Created by filipe haniel on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexSwiftTests/PokedexSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokedexSwiftTests.swift
+//  PokedexSwiftTests
+//
+//  Created by filipe haniel on 20/10/24.
+//
+
+import Testing
+
+struct PokedexSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
